b00fcaa4c40388531c988bc18bffe9bbfdfacce7
Aditya Tomar <tomaraditya481@gmail.com>
2026-09-18 23:25:58 +0530

    added lab2/file2.js

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,33 +1,46 @@
 import http from "http";
-import { getusers} from"./user.js" ;
+
 const server = http.createServer((req, res) => {
-  if ((req.url === "/api/users" && req.method === "GET")) {
-    res.end(JSON.stringify({ msg: "all users" }));
-  } 
-  else if ((req.url === "/api/users" && req.method === "POST")) {
-    let body ='';
-    req.on('data',(chunk)=>{
-       body += chunk
-    })
-    req.on('end',()=>{
-      const user =JSON.parse(body);
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.end(JSON.stringify({ msg: "Show users" }));
+
+  } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const user = JSON.parse(body);
       console.log(user);
-    })
-    res.end(JSON.stringify({ msg: "add user" }));
-  }
-  else if ((req.url === "/api/users/1"&& req.method === "GET")) {
-    res.end(JSON.stringify({ msg: "single user with Id 1" }));
-  }
-  else if ((req.url === "/api/users/1" && req.method === "PUT")) {
-    res.end(JSON.stringify({ msg: "update user 1" }));
-  }
-  else if ((req.url === "/api/users/1" && req.method === "DELETE")) {
-    res.end(JSON.stringify({ msg: "remove 1" }));
-  }
-  else {
+
+      res.end(
+        JSON.stringify({
+          msg: "Add user",
+          user: user,
+        })
+      );
+    });
+
+  } else if (req.url === "/api/users/1" && req.method === "GET") {
+    res.end(JSON.stringify({ msg: "Show user with id 1" }));
+
+  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+    res.end(JSON.stringify({ msg: "Replace user 1" }));
+
+  } else if (req.url === "/api/users/1" && req.method === "DELETE") {
+    res.end(JSON.stringify({ msg: "Delete user 1" }));
+
+  } else if (req.url === "/api/users/1" && req.method === "PATCH") {
+    res.end(JSON.stringify({ msg: "Partially update user 1" }));
+
+  } else {
     res.statusCode = 404;
     res.end();
   }
 });
 
-server.listen(3000,()=> {console.log("prg7 is running")});
+server.listen(5555, () => {
+  console.log("Server prg7 running ...");
+});
